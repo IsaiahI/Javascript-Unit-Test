be3b764b7934dc0c492d34582eba2b76513d9f72
hainzl17 <54989560+hainzl17@users.noreply.github.com>
2020-01-03 17:10:00 -0500

    Update task2.js

--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,8 +1,10 @@
 /* Put code for the task below */
 function task2(){
-
-
-
-
-
+  document.getElementById("table1").style.backgroundColor = "red";
+}
+function task2A(){
+  document.getElementById("table1").style.backgroundColor = "yellow";
+}
+function task2B(){
+  document.getElementById("table1").style.backgroundColor = "green";
 }
