9cd5ec7041b99462452f7309001668eb44411eb2
Ingino <20249@vtsd.com>
2020-01-03 11:38:29 -0500

    1-3-19

--- assets/javascript/task4.js
+++ assets/javascript/task4.js
@@ -1,1 +1,7 @@
-/* Put code for the task below */
+/* Put code for the task below|||||Check that all fields within the form are filled out. In addition, verify the validity of the phone number field (make sure it has exactly 10 characters).*/
+function phone(){
+  var name = document.getElementById("phone").value;
+  if(name.length < 5 || name.length > 25){
+    alert("enter a valid name");
+  }
+}
