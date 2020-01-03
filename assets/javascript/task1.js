f967ebc37dde8a6db3374a2433a667d712ad4c0e
hainzl17 <54989560+hainzl17@users.noreply.github.com>
2020-01-03 14:43:48 -0500

    Update task1.js

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -3,5 +3,5 @@ function taskOne(){
 document.getElementById("buttonTask1").style.display = "block";
 }
 function imageDisappear(){
-	
+document.getElementById("imageTask1").style.visibility = "hidden";
 }
