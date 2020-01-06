6a53e3412e46ae03b78f0a3ce0378df15448b4ef
Ingino <20249@vtsd.com>
2020-01-06 11:38:58 -0500

    1-6-20

--- assets/javascript/task4.js
+++ assets/javascript/task4.js
@@ -1,7 +1,17 @@
 /* Put code for the task below|||||Check that all fields within the form are filled out. In addition, verify the validity of the phone number field (make sure it has exactly 10 characters).*/
 function phone(){
   var name = document.getElementById("phone").value;
-  if(name.length < 5 || name.length > 25){
+  if(name.length !== 10 ){
+    /*sorce used for !== */
     alert("Not a Valid Phone Number");
   }
+}
+function length(){
+  var name = document.getElementById("length3").value;
+  var address = document.getElementById("length2").value;
+  var email = document.getElementById("length1").value;
+  var letters = /^[A-Za-z]+$/;
+  if(inputtxt.value.match(letters){
+      alert('Your name have accepted : you can try another');}
+
 }
